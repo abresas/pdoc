4d7f3875a332b4f5c5f90f6c3b600d71d7a30017
Alexios Theodoros Brezas <abresas@gmail.com>
2018-07-13 01:00:35 +0300

    remove hardcoded directory

--- cli/pdoc.php
+++ cli/pdoc.php
@@ -6,6 +6,11 @@ use PDoc\DocumentationGenerator;
 use PDoc\DocumentationWriter;
 
 $dirPath = '/home/abresas/dev/pdoc/lib';
+if (count($argv) < 2) {
+    echo "Usage: " . $argv[0] . " SOURCE_DIR\n";
+    exit(1);
+}
+$dirPath = realpath($argv[1]);
 $generator = new DocumentationGenerator();
 $namespaces = $generator->parseDirectory($dirPath);
 $writer = new DocumentationWriter();
