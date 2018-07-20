2c7cb6a7c354833f0e70dfbe2e3f4dcf8f488b95
Alexios Theodoros Brezas <abresas@gmail.com>
2018-07-20 22:44:48 +0300

    cli optionally pass target path

--- cli/pdoc.php
+++ cli/pdoc.php
@@ -11,10 +11,15 @@ if (count($argv) < 2) {
     echo "Usage: " . $argv[0] . " SOURCE_DIR\n";
     exit(1);
 }
+$targetPath = $argv[2] ?? dirname(__DIR__) . '/docs';
+if ($targetPath[0] !== '.' && $targetPath[0] !== '/') {
+    $targetPath = './' . $targetPath;
+}
 $dirPath = realpath($argv[1]);
 $dirScanner = new DirectoryScanner();
 $files = $dirScanner->scan($dirPath, '/^.*\.php$/');
 $generator = new DocumentationGenerator();
 $documentation = $generator->parseFiles($files);
 $writer = new DocumentationWriter();
-$writer->write($documentation);
+
+$writer->write($documentation, $targetPath);
